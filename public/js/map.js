fd8f2771b5fa4e22e48ee3f2e92ac94a531ba375
Tom Anderson <tom@ando.gq>
2020-03-24 20:43:32 +1100

    Basic Geolocation

--- public/js/map.js
+++ public/js/map.js
@@ -1,17 +1,22 @@
 class Map {
-    constructor(id, center, zoom) {
+    constructor({id, center, zoom, source, showZoom}) {
+        this.view = new ol.View({
+            center: center.proj,
+            zoom: zoom
+        });
+
         this.map = new ol.Map({
             target: id,
             layers: [
                 new ol.layer.Tile({
-                    source: new ol.source.OSM()
+                    source: new ol.source.OSM({url: source})
                 })
             ],
-            view: new ol.View({
-                center: center.proj,
-                zoom: zoom
+            view: this.view,
+            controls: ol.control.defaults({
+                zoom: showZoom == undefined ? true : showZoom
             })
-        })
+        });
     }
 
     addLayer(layer) {
--- public/main.js
+++ public/main.js
@@ -1,16 +1,41 @@
-let map = new Map("map", new Coord(144.9631,-37.8136), 15);
+let ausCoords = new Coord(133.7751, -25.2744);
+
+let map = new Map({
+    id: "map",
+    center: ausCoords,
+    zoom: 3,
+    showZoom: false
+});
 let heatmap = new Heatmap(map);
 let network = new Network(map, "red", 1);
 
-let data;
-
-let xhr = new XMLHttpRequest();
-xhr.onload = () => {
-    data = JSON.parse(xhr.responseText);
-
-    data.forEach((line) => {
-        network.addLine(line.map(p => new Coord(...p)));
-    });
+function getCenter() {
+    return ol.proj.toLonLat(map.view.getCenter());
 }
-xhr.open("GET", "/data.json");
-xhr.send();
+
+let geo = new ol.Geolocation({
+    tracking: true,
+    trackingOptions: {
+        enableHighAccuracy: true
+    },
+    projection: map.view.getProjection()
+});
+
+geo.on("change", () => {
+    map.view.animate({
+        center: geo.getPosition(),
+        zoom: 12
+    }, 1);
+})
+
+// let data;
+// let xhr = new XMLHttpRequest();
+// xhr.onload = () => {
+//     data = JSON.parse(xhr.responseText);
+
+//     data.forEach((line) => {
+//         network.addLine(line.map(p => new Coord(...p)));
+//     });
+// }
+// xhr.open("GET", "/data.json");
+// xhr.send();
