08d9a9b22329bdffca960a5c2305e277a4003e7a
Tom Anderson <tom@ando.gq>
2020-04-25 19:12:31 +1000

    Validate data from the user

--- functions/index.js
+++ functions/index.js
@@ -13,18 +13,31 @@ exports.report = functions.https.onRequest((req, res) => {
             try {
                 data = JSON.parse(req.body);
 
-            } catch {
+                // Make everything into the write types
+                data.stopId = Number(data.stopId);
+                data.time = new Date(data.time);
+                data.amount = Number(data.amount);
+
+                let stopIdValid = !isNaN(data.stopId);
+                let amountValid = !isNaN(data.amount) && data.amount > 0 && data.amount <= 4;
+
+                // Only accept dates for previous hour and next 5 minutes
+                let min = new Date(Date.now() - (1000 * 60 * 60)); // 1 hour in the past
+                let max = new Date(Date.now() + (1000 * 60 * 5)); // 5 minutes in the future
+                let timeValid = data.time > min || data.time < max
+                // Error checking
+                if (!(stopIdValid && amountValid && timeValid)) throw("Problem with data");
+            } catch (e) {
                 reject();
             }
-            if (data != undefined && data.stopId != undefined && data.time != undefined && data.amount != undefined) {
-                db.collection("reports").add({
-                    amount: data.amount,
-                    stopId: data.stopId,
-                    time: data.time
-                }).then(() => {
-                    resolve();
-                });
-            } else reject();
+            
+            db.collection("reports").add({
+                amount: data.amount,
+                stopId: data.stopId,
+                time: data.time
+            }).then(() => {
+                resolve();
+            });
         } else reject();
     }).then((body = "") => {
         res.status(200).send(JSON.stringify(body));
