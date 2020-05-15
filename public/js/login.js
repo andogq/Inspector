cbe52e05f73e436e9e252e19e15f5dce13d139ed
Tom Anderson <tom@ando.gq>
2020-05-15 12:25:37 +1000

    Retry verification code after incorrect attempt

--- public/js/login.js
+++ public/js/login.js
@@ -45,18 +45,18 @@ function verifyCode() {
         state.set("map");
         g.loggedIn = true;
         firebase.analytics().logEvent("login", {method: "phone"});
-    }).catch((err) => {
-        console.error(err);
-        notification.set("Incorrect verification code");
-    }).finally(() => {
-        // Swap the login page back
+
+        // Everything went well, so reset the form
         dom.page.login.classList.remove("step2");
         dom.page.login.classList.add("step1");
 
         // Destroy the verifier
         g.login.verifier.clear();
         g.login = {};
-
+    }).catch((err) => {
+        console.error(err);
+        notification.set("Incorrect verification code, try again");
+    }).finally(() => {
         load.stop(verifyLoadId);
         dom.button.verify.disabled = false;
     });
