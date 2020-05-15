d8ebbb4731b361346fb934fea356c43851e16cb7
Tom Anderson <tom@ando.gq>
2020-05-15 12:22:17 +1000

    Ensure the latest position is retrieved when recentering

--- public/js/geo.js
+++ public/js/geo.js
@@ -2,7 +2,10 @@
 function getCurrentPosition() {
     return new Promise((resolve, reject) => {
         if (navigator.geolocation == undefined) reject();
-        else navigator.geolocation.getCurrentPosition(resolve, reject);
+        else navigator.geolocation.getCurrentPosition(resolve, reject, {
+            enableHighAccuracy: true,
+            maximumAge: 0
+        });
     }).then((pos) => {
         return {
             lon: pos.coords.longitude,
